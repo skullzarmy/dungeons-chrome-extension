66909fc26dcbb02309f6359adc10b0df0f840f47
skllzrmy.tez <joe@poundfit.com>
2023-05-18 21:08:29 -0700

    ✨ feat(README.md): add instructions for installing and using the Dungeons Status Chrome Extension ✨ feat(README.md): add a description of the Dungeons Status Chrome Extension and its purpose ✨ feat(README.md): add a LICENSE section to the README file ✨ feat(README.md): add a LOGO COPYRIGHT section to the README file ✨ feat(icons): add icons for the Dungeons Status Chrome Extension ✨ feat(manifest.json): add manifest file for the Dungeons Status Chrome Extension ✨ feat(popup.html): add popup HTML file for the Dungeons Status Chrome Extension ✨ feat(popup.js): add popup JavaScript file for the Dungeons Status Chrome Extension
    ✨ feat(styles.css): add styles for the app layout, header, content, footer, buttons, poll options, progress bars, and animations. Also, add support for light and dark mode based on user's preference.

---
+++ README.md
@@ -0,0 +1,61 @@
+# Dungeons Character Dashboard: Chrome Extension
+
+A Manifest V3 Chrome Extension which loads current status information from [@dungeons@mastodon.social](https://mastodon.social/@dungeons)
+
+[Dungeons](https://dungeons.astrelion.com/) is an open-source D&D 5e-style RPG, played via community poll, on Mastodon. It was created and is maintained by [Astrelion](https://mastodon.social/@astrelion). This project is NOT affiliated with Astrelion or [@dungeons](https://mastodon.social/@dungeons).
+
+Follow [@Dungeon_Guide](https://mastodon.social/@dungeon_guide) a GPT-based Mastodon bot I created to help (it's GPT-3 so curb your expectations) the Dungeons community!
+
+<img src="icons/extension-example.png" alt="Example of Chrome extension" width="200">
+
+### Required Permissions:
+
+None
+
+## Installation
+
+0.  Clone this repo to your local machine
+
+```bash
+git clone https://github.com/skullzarmy/dungeons-chrome-extension.git
+```
+
+1.  Open Chrome and navigate to `chrome://extensions` by typing it into the address bar and hitting enter.
+
+1.  At the top right [↗] of the page, toggle on "Developer mode". This will enable additional options for loading in custom extensions.
+
+    <img src="icons/enable-dev.png" alt="Enable Developer Mode" width="150">
+
+1.  At the top left [↖] of the page, click on the "Load unpacked" button that appears once you have enabled developer mode.
+
+    <img src="icons/load-unpacked.png" alt="Load Unpacked" width="150">
+
+1.  A file selection dialog will appear. Navigate to the directory where you cloned the repo, and select the directory.
+
+The extension should now appear in your list of extensions and be available for use. You should see its icon [ ![Extension Icon](icons/icon16.png) ] in the extensions section of the Chrome toolbar. If the icon doesn't appear, you may need to click on the Extensions icon (which looks like a puzzle piece) in the Chrome toolbar and pin your extension so that its icon is always visible.
+
+If you download a new version, first replace the files in the extension directory with the new versions, then you can click the "Reload" button on your extension's box in the chrome://extensions page to load in the changes.
+
+Remember, your extension will only be available in the current profile and on the current device where you loaded it. If you sign in to a different Chrome profile, or use a different device, you'll need to load the extension again.
+
+## LICENSE
+
+MIT License
+
+Copyright (c) 2023 skllzrmy [Joe Peterson](https://joepeterson.work)
+
+[LICENSE FILE](LICENSE)
+
+### LOGO COPYRIGHT
+
+![D brand logo](icons/icon48.png) (D Brand Logo) is wholly owned and copyrighted as follows:
+
+Copyright (C) 2023 ASTRELION [License](https://gitlab.com/ASTRELION/dungeons-bot/-/raw/main/LICENSE)
+
+Original source: https://gitlab.com/ASTRELION/dungeons-bot
+
+Original account: https://mastodon.social/@dungeons
+
+**This mark is used without express permission or license.**
+
+**Use of this mark is not intended to convey affiliation with Astrelion**
---
+++ popup.js
@@ -0,0 +1,110 @@
+window.onload = () => {
+    // Fetch the note
+    fetchAndDisplayData();
+    setInterval(fetchAndDisplayData, 15000); // fetch and update data every 15 seconds
+};
+
+async function fetchAndDisplayData() {
+    const dungeonsId = await getDungeonsId();
+    const poll = await fetchLatestPoll(dungeonsId);
+    const content = document.getElementById("content");
+
+    // Fetch the account note
+    const response = await fetch("https://botsin.space/api/v1/accounts/lookup?acct=dungeons@mastodon.social");
+    const data = await response.json();
+
+    const newContent = document.createElement("div");
+
+    const char = document.createElement("div");
+    char.id = "character-data";
+    char.classList.add("character-data");
+    char.innerHTML = data.note;
+    newContent.appendChild(char);
+
+    if (poll) {
+        // Fetch detailed poll data
+        const pollResponse = await fetch(`https://botsin.space/api/v1/polls/${poll.poll.id}`);
+        const pollData = await pollResponse.json();
+
+        // Generate progress bars for each poll option
+        const totalVotes = pollData.options.reduce((total, option) => total + option.votes_count, 0);
+
+        const countdown = document.createElement("h4");
+        countdown.id = "countdown";
+        countdown.innerHTML = "Checking for new poll data...";
+        const countDownDate = new Date(pollData.expires_at).getTime();
+        countdown.innerHTML = generateCountdown(countDownDate);
+        newContent.appendChild(countdown);
+
+        const pollContent = document.createElement("div");
+        pollContent.classList.add("poll-content");
+        pollContent.innerHTML = `<h3>${poll.content}</h3>`;
+        newContent.appendChild(pollContent);
+
+        const votesCount = document.createElement("h3");
+        votesCount.innerHTML = `${pollData.votes_count} votes`;
+        newContent.appendChild(votesCount);
+
+        const progressBarContainer = document.createElement("div");
+        for (let option of pollData.options) {
+            const progressBar = generateProgressBar(option, totalVotes);
+            progressBarContainer.innerHTML += progressBar;
+        }
+        newContent.appendChild(progressBarContainer);
+
+        const link = document.createElement("a");
+        link.href = poll.url;
+        link.classList.add("button");
+        link.innerHTML = `VOTE NOW ON&nbsp;<i class="mastodon"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-mastodon" viewBox="0 0 16 16"><path d="M11.19 12.195c2.016-.24 3.77-1.475 3.99-2.603.348-1.778.32-4.339.32-4.339 0-3.47-2.286-4.488-2.286-4.488C12.062.238 10.083.017 8.027 0h-.05C5.92.017 3.942.238 2.79.765c0 0-2.285 1.017-2.285 4.488l-.002.662c-.004.64-.007 1.35.011 2.091.083 3.394.626 6.74 3.78 7.57 1.454.383 2.703.463 3.709.408 1.823-.1 2.847-.647 2.847-.647l-.06-1.317s-1.303.41-2.767.36c-1.45-.05-2.98-.156-3.215-1.928a3.614 3.614 0 0 1-.033-.496s1.424.346 3.228.428c1.103.05 2.137-.064 3.188-.189zm1.613-2.47H11.13v-4.08c0-.859-.364-1.295-1.091-1.295-.804 0-1.207.517-1.207 1.541v2.233H7.168V5.89c0-1.024-.403-1.541-1.207-1.541-.727 0-1.091.436-1.091 1.296v4.079H3.197V5.522c0-.859.22-1.541.66-2.046.456-.505 1.052-.764 1.793-.764.856 0 1.504.328 1.933.983L8 4.39l.417-.695c.429-.655 1.077-.983 1.934-.983.74 0 1.336.259 1.791.764.442.505.661 1.187.661 2.046v4.203z"/></svg></i>`;
+        newContent.appendChild(link);
+    }
+
+    // Replace the content with the new content
+    content.innerHTML = newContent.innerHTML;
+
+    // Make all links open in a new tab
+    const links = document.getElementById("app").querySelectorAll("a");
+    links.forEach((link) => {
+        link.addEventListener("click", (event) => {
+            event.preventDefault();
+            chrome.tabs.create({ url: event.currentTarget.href });
+        });
+    });
+}
+
+async function getDungeonsId() {
+    const dungeonsResponse = await fetch("https://botsin.space/api/v1/accounts/lookup?acct=dungeons@mastodon.social");
+    const dungeonsData = await dungeonsResponse.json();
+    return dungeonsData.id;
+}
+
+async function fetchLatestPoll(dungeonsId) {
+    const statusResponse = await fetch(`https://botsin.space/api/v1/accounts/${dungeonsId}/statuses`);
+    const data = await statusResponse.json();
+    return data.find((status) => status.poll && !status.poll.expired);
+}
+
+function generateProgressBar(option, totalVotes) {
+    return `<div class="poll-option">
+        <span class="option-title">${option.title} (votes: ${option.votes_count})</span>
+        <div class="progress-bar">
+            <div class="progress-bar-fill" style="flex-grow: ${option.votes_count}"></div>
+            <div class="progress-bar-remaining" style="flex-grow: ${totalVotes - option.votes_count}"></div>
+        </div>
+    </div>`;
+}
+
+function generateCountdown(countDownDate) {
+    const x = setInterval(() => {
+        const now = new Date().getTime();
+        const distance = countDownDate - now;
+        const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+        const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+        document.getElementById("countdown").innerHTML = `ENDS IN: ${minutes}m ${seconds}s`;
+        if (distance < 0) {
+            clearInterval(x);
+            document.getElementById("countdown").innerHTML = `ENDED`;
+        }
+    }, 1000);
+    return "Checking for poll expiration...";
+}
